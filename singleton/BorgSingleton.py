d2c74b7e4f54265e8887f8d7c673f444bd2f3c0a
M Reza Nurrifqi <reza.rifqi@yahoo.com>
2017-01-05 16:44:13 +0700

    add explanation at code

--- singleton/BorgSingleton.py
+++ singleton/BorgSingleton.py
@@ -1,9 +1,9 @@
 class Borg(object):
-	_shared_state = {}
+	_shared_state = {} #variable to save state every class instantiated
 
 	def __new__(cls, *args, **kwargs):
-		obj = super(Borg, cls).__new__(cls, *args, **kwargs)
-		obj.__dict__ = cls._shared_state
+		obj = super(Borg, cls).__new__(cls, *args, **kwargs) #return value of __new__ from parent class for reference
+		obj.__dict__ = cls._shared_state #save value of shared_state to __dict__
 		return obj
 
 class Child(Borg):
@@ -11,6 +11,7 @@ class Child(Borg):
 
 borg = Borg()
 another = Borg()
+print borg._shared_state
 
 compare = borg is another
 print compare
--- singleton/ClassicSingleton.py
+++ singleton/ClassicSingleton.py
@@ -1,7 +1,11 @@
 class ClassicSingleton(object):
-	def __new__(cls):
-		if not hasattr(cls, 'instance'):
-			cls.instance = super(ClassicSingleton, cls).__new__(cls)
+	'''
+		__new__ is is a static method (special-cased so you need not declare it as such) 
+		that takes the class of which an instance was requested as its first argument
+	'''
+	def __new__(cls): 
+		if not hasattr(cls, 'instance'): #check if class has not instantiated before
+			cls.instance = super(ClassicSingleton, cls).__new__(cls) #return value of __new__ from parent class for reference
 		return cls.instance
 
 first_singleton = ClassicSingleton()
